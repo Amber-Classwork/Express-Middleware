e2fcc643e60f2460980bab88bcf9d98724c1ded3
aldaine-AG <aldaineclarke1@gmail.com>
2022-07-28 06:34:28 -0500

    fixed code so after deleting record it will update students list

--- student/src/app/home/home.component.ts
+++ student/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Student, StudentsService } from '../Services/students.service';
 
 @Component({
@@ -10,22 +11,23 @@ export class HomeComponent implements OnInit {
 
   constructor(private studentsService: StudentsService) { }
 
-  students:Student[] = [];
-  patchStudent = {name: "Donald Junior"}
+  students$!:Observable<Student[]>;
+
   ngOnInit(): void {
     this.getAllStudents();
     console.log("getAllStudents");
-    this.updateStudentName("62e1969c66cc2356d506510a", this.patchStudent)
+
   }
 
   getAllStudents(){
-    this.studentsService.getAllStudent().subscribe((data)=>{
-      this.students = data;
-    })
+    this.students$ = this.studentsService.getAllStudent()
   }
   delStudent(studentId: string){
-    this.studentsService.deleteStudent(studentId).subscribe();
-    this.students = this.students.filter((student)=> student._id !== studentId);
+    this.studentsService.deleteStudent(studentId).subscribe(()=>{
+      this.getAllStudents()
+    });
+
+    
   }
   updateStudentName(studentId:string, patched:Partial<Student>){
     this.studentsService.updateStudent(studentId, patched).subscribe();
